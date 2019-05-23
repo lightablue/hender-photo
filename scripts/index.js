e476ab951ce0a3e2a98a96205511a7d936e24b71
lightablue <a8674109@stmail.com.tw>
2019-05-23 21:02:28 +0800

    Fix CORS by Whatever Origin

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,6 @@
 document.getElementById("app").innerHTML = `<h1>Hello HenderPhoto!</h1>`;
 
-fetch("https://www.google.com.tw/")
-.then(function(res) {
-  return res.text();
-})
-.then(function(html) {
-  console.log(`html = ${html}`);
+var url = "http://www.whateverorigin.org/get?url=" + encodeURIComponent("http://www.wces.tp.edu.tw/happy/102rainbow/photo/pt_show.asp?pt_id=47523") + "&callback=?";
+$.getJSON(url, function(response) {
+  console.log(response);
 });
-/*
- * Messages in Console When the First Time Load the index.html
- * polyfill.min.js:1 Failed to load resource: net::ERR_FILE_NOT_FOUND
- * index.html:1 Access to fetch at 'https://www.google.com.tw/' from origin 'null' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
- * index.html:1 Uncaught (in promise) TypeError: Failed to fetch
- */
-/*
- * Messages in Console after Reload
- * GET file://cdn.polyfill.io/v2/polyfill.min.js?features=fetch net::ERR_FILE_NOT_FOUND
- * index.html:1 Access to fetch at 'https://www.google.com.tw/' from origin 'null' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
- * index.html:1 Uncaught (in promise) TypeError: Failed to fetch
- * Promise.then (async)
- * (anonymous) @ index.js:7
- * index.js:3 Cross-Origin Read Blocking (CORB) blocked cross-origin response https://www.google.com.tw/ with MIME type text/html. See https://www.chromestatus.com/feature/5629709824032768 for more details.
- */
