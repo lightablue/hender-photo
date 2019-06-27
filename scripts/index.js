b064ccfe803c2b973ca185f70a7ce2ed11adbbe4
lightablue <a8674109@stmail.com.tw>
2019-06-28 01:14:14 +0800

    Show photos in photo grid by flexbin

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ const app = document.getElementById("app");
 const timeline = document.createElement("DIV");
 timeline.setAttribute("class", "timeline");
 app.appendChild(timeline);
+const album = document.getElementById("album")
 const schoolWebSite = "http://www.wces.tp.edu.tw";
 const classFolder = "/happy/102rainbow/";
 const viewerFolder = "photo/";
@@ -9,12 +10,8 @@ const fileFolder = "files/";
 const galleryPage = "pa_all.asp";
 const albumPage = "pa.asp";
 const albumParam = "mainid";
-const albumId = 1563;
 const photoPage = "pt_show.asp";
 const photoParam = "pt_id";
-const encodedUri = encodeURIComponent(`${schoolWebSite}${classFolder}${viewerFolder}${galleryPage}`);
-//const encodedUri = encodeURIComponent(`${schoolWebSite}${classFolder}${viewerFolder}${albumPage}?${albumParam}=${albumId}`);
-const url = `http://www.whateverorigin.org/get?url=${encodedUri}&callback=?`;
 function parseToAlbumInfo(row) {
     const columns = row.querySelectorAll('td');
     const anchor = columns[0].querySelector(`a[href^='${albumPage}?${albumParam}=']`);
@@ -48,32 +45,63 @@ function addToTimeline(albumInfos) {
         timeline.appendChild(container);
     });
 }
+function getUrl(encodedUri) {
+    return `http://www.whateverorigin.org/get?url=${encodedUri}&callback=?`;
+}
+function parseToBody(response) {
+  /* console.log(response);
+  console.log(response.contents); */
+  const parser = new DOMParser();
+  return parser.parseFromString(response.contents, response.status.content_type).body;
+}
+var albumId;
+function parseToPhotoInfo(img) {
+    const src = img.src;
+    const thumbnailFileName = src.substring(src.lastIndexOf("/")+1);
+    const fileName = thumbnailFileName.replace("_small", "");
+    return `${schoolWebSite}${classFolder}${fileFolder}${albumId}/${fileName}`;
+}
+function parseToPhotoInfos(body) {
+    const photoInfos = [];
+    const imgs = body.querySelectorAll(`img[src^='../${fileFolder}${albumId}/']`);
+    //console.log(imgs);
+    imgs.forEach((img) => photoInfos.push(parseToPhotoInfo(img)));
+    return photoInfos;
+}
+function addToAlbum(photoInfos) {
+    const flexbin = document.createElement("DIV");
+    flexbin.setAttribute("class", "flexbin");
+    photoInfos.forEach((photoInfo) => {
+        const anchor = document.createElement("A");
+        anchor.setAttribute("href", photoInfo);
+        const img = document.createElement("IMG");
+        img.setAttribute("src", photoInfo);
+        flexbin.appendChild(anchor);
+        anchor.appendChild(img);
+    });
+    album.appendChild(flexbin);
+}
+const callbackForFetchAlbum = function(response) {
+    album.style.width = "100%";
+    const body = parseToBody(response);
+    addToAlbum(parseToPhotoInfos(body));
+}
 function openAlbum(albumInfo) {
-    console.log(albumInfo.id);
-    document.getElementById("album").style.width = "100%";
+    albumId = albumInfo.id;
+    const encodedUri = encodeURIComponent(`${schoolWebSite}${classFolder}${viewerFolder}${albumPage}?${albumParam}=${albumId}`);
+    $.getJSON(getUrl(encodedUri), callbackForFetchAlbum);
 }
 function returnToGallery() {
-    document.getElementById("album").style.width = "0";
+    album.removeChild(album.querySelector(".flexbin"));
+    album.style.width = "0";
 }
-const callback = function(response) {
-  console.log(response);
-  console.log(response.contents);
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(response.contents, response.status.content_type);
-  /* const selected = doc.body.querySelector("select[name='pages']");
+const callbackForFetchGallery = function(response) {
+  const body = parseToBody(response);
+  /* const selected = body.querySelector("select[name='pages']");
   console.log(selected); */
-  /* const selected = doc.body.querySelectorAll(`a[href^='${albumPage}?${albumParam}=']`);
-  console.log(selected);
-  console.log(selected[0]);
-  console.log("'" + selected[0].text.trim() + "', " + selected[0].text.trim().length);
-  console.log((new URLSearchParams(selected[0].search)).get(albumParam)); */
-  /* const selected = doc.body.querySelectorAll(`a[href^='${photoPage}?${photoParam}=']`);
-  console.log(selected);
-  const src = selected[0].querySelector(`img[src^='../${fileFolder}${albumId}/']`).src;
-  const fileName = src.substring(src.lastIndexOf("/")+1);
-  console.log(fileName.replace("_small", "")); */
-  const albumInfos = parseToAlbumInfos(doc.body);
-  console.log(albumInfos);
+  const albumInfos = parseToAlbumInfos(body);
+  //console.log(albumInfos);
   addToTimeline(albumInfos);
 }
-$.getJSON(url, callback);
+const encodedUri = encodeURIComponent(`${schoolWebSite}${classFolder}${viewerFolder}${galleryPage}`);
+$.getJSON(getUrl(encodedUri), callbackForFetchGallery);
